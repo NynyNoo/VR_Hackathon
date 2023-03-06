506b98820e6ed192ee0592421d246d861329c250
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-06 22:01:09 +0100

    Removed obsolete comments

--- VR Hackathon/Assets/Custom Content/Scripts/MultipleObjectsToColor.cs
+++ VR Hackathon/Assets/Custom Content/Scripts/MultipleObjectsToColor.cs
@@ -5,34 +5,11 @@ public class MultipleObjectsToColor : MonoBehaviour
     [SerializeField]
     public Colorable[] Colorables;
 
-    private void Start()
-    {
-        //Colorables = gameObject.GetComponentsInChildren<Colorable>();
-    }
     public void ChangeColorsOfThisShaders(float brushPower, Color brushColor)
     {
         foreach(var colorable in Colorables)
         {
             colorable.ChangeColor(brushPower, brushColor);
         }
-
-        //foreach (var item in renderers)
-        //{
-        //    if(item.gameObject.tag == "Glass")
-        //    {
-        //        continue;
-        //    }
-
-        //    if(item.material.GetColor("_BrushColor") != brushColor)
-        //    {
-        //        item.material.SetFloat("_Intensity",  brushPower);
-        //        item.material.SetColor("_BrushColor", brushColor);
-        //    }
-        //    else
-        //    {
-        //        item.material.SetFloat("_Intensity", item.material.GetFloat("_Intensity") + brushPower);
-        //        item.material.SetColor("_BrushColor", brushColor);
-        //    } 
-        //}
     }
 }
