3e7d254ee953853c7dcaac6a499bfe5500dd6b78
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-06 22:41:06 +0100

    Made painting of a single object use Colorable class

--- VR Hackathon/Assets/Custom Content/Scripts/Coloring.cs
+++ VR Hackathon/Assets/Custom Content/Scripts/Coloring.cs
@@ -9,17 +9,8 @@ public class Coloring : MonoBehaviour
     {
         if (other.tag == "Colorable")
         {
-            Renderer rendererToPaint = other.GetComponent<Renderer>();
-            if (rendererToPaint.material.GetColor("_BrushColor") != brushRenderer.material.color)
-            {
-                rendererToPaint.material.SetFloat("_Intensity", brushPower);
-                rendererToPaint.material.SetColor("_BrushColor", brushRenderer.material.color);
-            }
-            else
-            {
-                rendererToPaint.material.SetFloat("_Intensity", rendererToPaint.material.GetFloat("_Intensity") + brushPower);
-                rendererToPaint.material.SetFloat("_Intensity", rendererToPaint.material.GetFloat("_Intensity") + brushPower);
-            }
+            Colorable colorable = other.GetComponent<Colorable>();
+            colorable.ChangeColor(brushPower, brushRenderer.material.color);
         }
 
         if (other.tag == "MultipleColorable")
