2d1f7aa45747a6255cdb6be1f851f559be3dd2ed
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-07 23:53:06 +0100

    Made character move to a random point instead of zero

--- VR Hackathon/Assets/CharacterWanderBehaviour.cs
+++ VR Hackathon/Assets/CharacterWanderBehaviour.cs
@@ -7,10 +7,27 @@ public class CharacterWanderBehaviour : MonoBehaviour
 {
     [SerializeField]
     private NavMeshAgent _navMeshAgent;
+    [SerializeField]
+    private float _maxWalkDistance = 10f;
+
+    private Vector3 _currentDestination;
 
     void Start()
     {
-        _navMeshAgent.destination = Vector3.zero;
+        _navMeshAgent.destination = GetRandomPositionOnNavmesh();
         _navMeshAgent.isStopped = false;
     }
+
+    private Vector3 GetRandomPositionOnNavmesh()
+    {
+        Vector3 direction = Random.insideUnitSphere * _maxWalkDistance;
+        direction += transform.position;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(direction, out hit, Random.Range(0f, _maxWalkDistance), 1);
+
+        return hit.position;
+    }
+
+
 }
