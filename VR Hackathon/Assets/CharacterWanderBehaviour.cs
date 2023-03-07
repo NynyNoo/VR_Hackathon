565be30359d397aa790703b84715a6ae8cef2b85
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-08 00:19:22 +0100

    Met 7 implement lifeless walking people placeholders (#22)
    * Set objects as navigation static and baked mesh
    
    * Set buildings as Navigation static and rebaked navmesh
    
    * Created empty prefab for a character
    
    * Imported Kenney characters
    
    * Added character model, empty script and a navmesh agent to prefab
    
    * Made character prefab attempt to walk to Zero at start
    
    * Made character move to a random point instead of zero
    
    * Implemented random movement with waiting
    
    * Made min and max wait time serialized
    
    * Fixed formatting

---
+++ VR Hackathon/Assets/CharacterWanderBehaviour.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterWanderBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _navMeshAgent;
+    [SerializeField]
+    private float _maxWalkDistance = 10f;
+    [SerializeField]
+    private float _minWaitTime = 5f;
+    [SerializeField]
+    private float _maxWaitTime = 20f;
+
+    public bool IsWaiting;
+
+    private Vector3 _currentDestination;
+
+    void Start()
+    {
+        _navMeshAgent.destination = GetRandomPositionOnNavmesh();
+        _navMeshAgent.isStopped = false;
+    }
+
+    private void Update()
+    {
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            if (IsWaiting == false)
+            {
+                StartCoroutine(WaitBeforeWandering());
+            }
+        }
+    }
+
+    private Vector3 GetRandomPositionOnNavmesh()
+    {
+        Vector3 direction = Random.insideUnitSphere * _maxWalkDistance;
+        direction += transform.position;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(direction, out hit, Random.Range(0f, _maxWalkDistance), 1);
+
+        return hit.position;
+    }
+
+    private IEnumerator WaitBeforeWandering()
+    {
+        IsWaiting = true;
+
+        float passedTime = 0;
+        float timeToWait = Random.Range(_minWaitTime, _maxWaitTime);
+
+        while (passedTime < timeToWait)
+        {
+            yield return null;
+            passedTime += Time.deltaTime;
+        }
+
+        GoToARandomPosition();
+    }
+
+    private void GoToARandomPosition()
+    {
+        _navMeshAgent.destination = GetRandomPositionOnNavmesh();
+        _navMeshAgent.isStopped = false;
+        IsWaiting = false;
+    }
+}
