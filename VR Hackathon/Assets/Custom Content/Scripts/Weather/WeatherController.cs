2aee7cf051f8675d3f748eed3c821dc9a48361dd
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-09 19:32:19 +0100

    Met 36 make the weather and lighting be affected by painting progress (#33)
    * Added WeatherController prefab with empty script
    
    * Prefabbed lights
    
    * Made MatiDevScene look like main scene
    
    * Subscribed to progress counter and written counter update handler
    
    * Created a skybox for the day
    
    * Removed old environment from devscene
    
    * Added directionallight to scene
    
    * Implemented basic weather controller
    
    * Made sun rise up when progress reached treshold
    
    * Fixed resetting of skybox color
    
    * Implemented turning off lights when day comes
    
    * Added disabling of fog

---
+++ VR Hackathon/Assets/Custom Content/Scripts/Weather/WeatherController.cs
@@ -0,0 +1,83 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField]
+    private Material _daySkyboxTemplate;
+    [SerializeField]
+    private Material _nightSkyboxTemplate;
+    [SerializeField]
+    private int _progressTresholdForSkyboxChange = 50;
+    [SerializeField]
+    private Transform _directionalLight;
+    [SerializeField]
+    private float _lightXRotationWhenSunUp = 15f;
+    [SerializeField]
+    private float _sunRisingTime = 5f;
+    [SerializeField]
+    private CityLightsController _cityLightsController;
+
+    private Material _currentSkybox;
+
+    private void Awake()
+    {
+        GameProgressCounter.CounterUpdated += HandleCounterUpdate;
+        _currentSkybox = RenderSettings.skybox;
+
+        ResetWeather();
+    }
+
+    private void HandleCounterUpdate(int progress)
+    {
+        if (progress > _progressTresholdForSkyboxChange)
+        {
+            ChangeWeatherToDay();
+            ChangeSunPosition();
+        }
+    }
+
+    private void ChangeSunPosition()
+    {
+        float y = _directionalLight.transform.rotation.eulerAngles.y;
+
+        _directionalLight.DORotate(
+            new Vector3(_lightXRotationWhenSunUp,
+            y, 0f), _sunRisingTime);
+    }
+
+    private void ChangeWeatherToDay()
+    {
+        CopyShaderColorParameter(_daySkyboxTemplate, _currentSkybox, "_SkyGradientTop");
+        _cityLightsController.BeginDisablingAllLights();
+        TurnOffFog();
+    }
+
+    private void CopyShaderColorParameter(Material from, Material to, string name)
+    {
+        to.DOColor(from.GetColor(name), name, 5f).From(to.GetColor(name));
+    }
+
+    private void TurnOffFog()
+    {
+        DOTween.To(() => RenderSettings.fogDensity,
+            (x) => RenderSettings.fogDensity = x,
+            0f, _sunRisingTime).SetEase(Ease.InCirc);
+    }
+
+    private void ResetWeather()
+    {
+        string changedParameterName = "_SkyGradientTop";
+
+        Color colorToCopy = _nightSkyboxTemplate.GetColor(changedParameterName);
+        _currentSkybox.SetColor(changedParameterName, colorToCopy);
+    }
+
+    private void OnApplicationQuit()
+    {
+        ResetWeather();
+    }
+}
