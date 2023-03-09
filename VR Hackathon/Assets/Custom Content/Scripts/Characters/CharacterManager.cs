7535edf053a7a75640f407a8667aee143e19ded4
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-09 11:49:06 +0100

    Made characters restore color after passed time

--- VR Hackathon/Assets/Custom Content/Scripts/Characters/CharacterManager.cs
+++ VR Hackathon/Assets/Custom Content/Scripts/Characters/CharacterManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class CharacterManager : MonoBehaviour
 {
     [SerializeField]
     private Renderer[] _characterRenderers;
+    [SerializeField]
+    private float _tweeningDuration = 5f;
 
     private float _passedTime;
 
@@ -18,17 +21,19 @@ public class CharacterManager : MonoBehaviour
     {
         if (_passedTime > 5f)
         {
-            RestoreColorToCharacters();
+            RestoreColorToCharacters(0.35f);
         }
 
         _passedTime += Time.deltaTime;
     }
 
-    private void RestoreColorToCharacters()
+    private void RestoreColorToCharacters(float intensity)
     {
         for (int i = 0; i < _characterRenderers.Length; i++)
         {
-            _characterRenderers[i].material.SetFloat("_Intensity", 100f);
+            DOTween.To(() => _characterRenderers[0].material.GetFloat("_Intensity"),
+                (x) => _characterRenderers[0].material.SetFloat("_Intensity", x),
+                intensity, 10f);
         }
     }
 }
