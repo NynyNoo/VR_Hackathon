9d1c827df6de20b27895e0dad78353b1f965a552
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-09 16:02:00 +0100

    Subscribed to progress counter and written counter update handler

--- VR Hackathon/Assets/Custom Content/Scripts/Weather/WeatherController.cs
+++ VR Hackathon/Assets/Custom Content/Scripts/Weather/WeatherController.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class WeatherController : MonoBehaviour
 {
+    [SerializeField]
+    private Material _currentSkybox;
+    [SerializeField]
+    private Material _daySkyboxTemplate;
+    [SerializeField]
+    private int _progressTresholdForSkyboxChange;
 
+    private void Start()
+    {
+        GameProgressCounter.CounterUpdated += HandleCounterUpdate;
+    }
+
+    private void HandleCounterUpdate(int progress)
+    {
+        if(progress > _progressTresholdForSkyboxChange)
+        {
+            ChangeSkyboxParameters();
+        }
+    }
+
+    private void ChangeSkyboxParameters()
+    {
+
+    }
 }
