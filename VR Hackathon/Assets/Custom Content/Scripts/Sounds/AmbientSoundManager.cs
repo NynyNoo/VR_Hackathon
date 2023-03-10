bc4fccdfbe57ab085f6fdd6bed898f0003ce72e4
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-10 10:28:33 +0100

    Configured audio manager

---
+++ VR Hackathon/Assets/Custom Content/Scripts/Sounds/AmbientSoundManager.cs
@@ -0,0 +1,49 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _nightAmbient;
+    [SerializeField]
+    private AudioSource _dayAmbient;
+    [SerializeField]
+    private float _dayAudioTweeningDuration;
+    [SerializeField]
+    private float _nightAudioTweeningDuration;
+    [SerializeField]
+    private float _progressThreshold;
+
+    private void Start()
+    {
+        GameProgressCounter.CounterUpdated += TrySwap;
+    }
+
+    private void TrySwap(int coloredObjects)
+    {
+        if(coloredObjects >= _progressThreshold)
+        {
+            SwapAmbientSounds();
+        }
+    }
+
+    private void SwapAmbientSounds()
+    {
+        _dayAmbient.Play();
+        TweenSourceVolume(_dayAmbient, 0f,
+            1f, _dayAudioTweeningDuration);
+
+        TweenSourceVolume(_nightAmbient,
+            1f, 0f, _nightAudioTweeningDuration);
+    }
+
+    private void TweenSourceVolume(AudioSource audioSource,
+        float startVolume, float targetVolume, float tweeningDuration)
+    {
+        audioSource.volume = startVolume;
+
+        audioSource.DOFade(targetVolume, tweeningDuration);
+    }
+}
