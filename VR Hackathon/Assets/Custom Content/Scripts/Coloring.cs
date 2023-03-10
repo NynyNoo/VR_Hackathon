d59d903a69aee3453c00a051461810a07f537c6c
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-10 11:53:27 +0100

    Met 24 add vf xand sfx to paintbrush when painting (#46)
    * Imported splat sounds
    
    * Added audio clips and source to marker
    
    * Added script that plays a random sound when painting
    
    * Created paint splash vfx
    
    * Made particles emit when something is being colored
    
    * Made particlesystem use world space and increased number of particles in a splash

--- VR Hackathon/Assets/Custom Content/Scripts/Coloring.cs
+++ VR Hackathon/Assets/Custom Content/Scripts/Coloring.cs
@@ -5,12 +5,21 @@ public class Coloring : MonoBehaviour
     [SerializeField] public Renderer brushRenderer;
     [SerializeField] public float brushSize;
     [SerializeField] public float brushPower;
+
+    [SerializeField]
+    private AudioClip[] _splatAudios;
+    [SerializeField]
+    private AudioSource _splatSource;
+    [SerializeField]
+    private ParticleSystem _splatParticles;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Colorable")
         {
             Colorable colorable = other.GetComponent<Colorable>();
             colorable.ChangeColor(brushPower, brushRenderer.material.color, true);
+            SplashPaint();
         }
 
         if (other.tag == "MultipleColorable")
@@ -21,8 +30,21 @@ public class Coloring : MonoBehaviour
             if (otherScript != null)
             {
                 otherScript.ChangeColorsOfThisShaders(brushPower, brushRenderer.material.color);
+                SplashPaint();
             }
         }
+    }
+
+    private void SplashPaint()
+    {
+        _splatParticles.Emit(5);
+        PlayRandomSplat();
+    }
+
+    private void PlayRandomSplat()
+    {
+        int index = Random.Range(0, _splatAudios.Length);
+        _splatSource.PlayOneShot(_splatAudios[index]);
     }
 
 }
