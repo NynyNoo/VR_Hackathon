ecfeed6615ffb8510b53cca29301550744efc745
MB <62397807+M-Bujak@users.noreply.github.com>
2023-03-10 11:10:40 +0100

    Added script that plays a random sound when painting

--- VR Hackathon/Assets/Custom Content/Scripts/Coloring.cs
+++ VR Hackathon/Assets/Custom Content/Scripts/Coloring.cs
@@ -17,6 +17,7 @@ public class Coloring : MonoBehaviour
         {
             Colorable colorable = other.GetComponent<Colorable>();
             colorable.ChangeColor(brushPower, brushRenderer.material.color, true);
+            PlayRandomSplat();
         }
 
         if (other.tag == "MultipleColorable")
@@ -27,8 +28,15 @@ public class Coloring : MonoBehaviour
             if (otherScript != null)
             {
                 otherScript.ChangeColorsOfThisShaders(brushPower, brushRenderer.material.color);
+                PlayRandomSplat();
             }
         }
+    }
+
+    private void PlayRandomSplat()
+    {
+        int index = Random.Range(0, _splatAudios.Length);
+        _splatSource.PlayOneShot(_splatAudios[index]);
     }
 
 }
